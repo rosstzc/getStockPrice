8e501f23a3329f8090d31872d49ad38a22eca313
mike <rosstzc@163.com>
2020-10-14 16:55:37 +0800

    可以正常使用，导出出各股票的季度折算股价

--- dataProcess.py
+++ dataProcess.py
@@ -8,7 +8,7 @@ def getStockPrice(data):
 
     # 定义"年季"字典，存储股价平均值
     yearDict = {}
-    data = data[::-1]
+    data = data[::-1]   #股票历史股价列表
     for i in data:
         # DF['DATE'] = pd.to_datetime(DF['DATE']).dt.to_period('Q')
         dateStr = i[0]
@@ -18,13 +18,13 @@ def getStockPrice(data):
         quarter = (data_time.month -1) //3 + 1
         yearQuarter = yearStr + "Q" + str(quarter)
         i[0] = yearQuarter
-        print( i[0] + "，" + i[1] +  "，" + i[5])
+        # print( i[0] + "，" + i[1] +  "，" + i[5])
 
         #把"年季"写入字典
         yearDict.update({i[0]: ""})
-    print(yearDict)
+    # print(yearDict)
 
-    # 计算季度平均股价
+    # 计算季度平均股价,并填入字典
     for key,value in yearDict.items():
         temp = 0
         k = 0
@@ -34,9 +34,9 @@ def getStockPrice(data):
                 temp = temp + priceClose
                 k = k + 1
         priceAverage = temp/k
-        print(str(priceAverage))
+        # print(str(priceAverage))
         yearDict[key] = str(priceAverage)  #给字典填上平均股价
-    # print(yearDict)
+    print(yearDict)
 
     
     #把各季度股价写入一个数组
